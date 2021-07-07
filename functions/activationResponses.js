5546117dba95fe3173076fc9a914f9eba9312192
L4P <lucky.for.people@gmail.com>
2021-07-07 14:03:20 +0300

    update html (phone, active, non active)

--- functions/activationResponses.js
+++ functions/activationResponses.js
@@ -3,232 +3,257 @@ const admin = require("firebase-admin");
 function getCss() {
  
   return `
-      <style>
+    <style>
       @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700;900&display=swap');
       body {
-    font-family: 'Roboto';
-    font-weight: 400;
-    color: rgb(0, 0, 0);
-    }
-    html{
-      height: 100%;
-      -webkit-box-sizing: border-box;
-      -moz-box-sizing: border-box;
-      box-sizing: border-box;
+        font-family: 'Roboto';
+        font-weight: 400;
+        color: rgb(0, 0, 0);
       }
+      html{
+        height: 100%;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        }
 
       @font-face {
-    font-family: "Haymaker";
-    font-style: normal;
-    font-weight: 400;
-    /* Браузер сначала попробует найти шрифт локально */
-    src: local("Haymaker"),
-    url("http://pgrm.pp.ua/fonts/Haymaker.woff") format("woff");
-    }
-    h1{
-    font-family: Haymaker,sans-serif !important;
-    font-weight: 100;
-    font-size: 50px;
-    margin: 20px auto;
-    text-align: center;
-    }
-    ::placeholder { 
-    color: #ccc !important;
-    font-size: 0.8rem;
-    font-weight: 100px;
-    }
-
-    .login {
-    min-width: 315px;
-    background: #F9F9F9;
-    padding: 20px;
-    border-radius: 15px;
-    display: block;
-    margin: 2px auto;
-    box-shadow: #0000002b 0px 0px 9px 2px;
-    max-width: 400px;
-    }
-    label.top_text {
-    display: block;
-    text-align: center;
-    color: #478698;
-    font-size: 20px;
-    font-weight: 900;
-    vertical-align: middle;
-    padding-bottom: 10px;
-    }
-    .top_text img{
-    width: 30px;
-    vertical-align: middle;
-    margin-right: 10px;
-    }
-    input.inp {
-    font-family: "MaisonNeue", "Segoe UI", "Helvetica Neue", -apple-system, system-ui, BlinkMacSystemFont, Roboto, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    font-weight: inherit;
-    color: #63578a !important;
-    font-weight: 600;
-    min-width: 80px;
-    display: block;
-    width: 100%;
-    height: 42px;
-    padding: 10px 30px;
-    font-size: 14px;
-    line-height: 1.42857143;
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #dfdfe8;
-    border-radius: 20px;
-    -webkit-box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%);
-    box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%);
-    -webkit-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
-    -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
-    transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
-    transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
-    transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
-    }
-    a.login_btn {
-    margin-top: 30px;
-    padding: 10px 30px;
-    background: #589aca;
-    border-radius: 5px;
-    display: block;
-    color: white;
-    font-weight: 100;
-    font-size: 15px;
-    }
-    .firebaseui-card-footer {
-          display: none;
+        font-family: "Haymaker";
+        font-style: normal;
+        font-weight: 400;
+        /* Браузер сначала попробует найти шрифт локально */
+        src: local("Haymaker"),
+        url("http://pgrm.pp.ua/fonts/Haymaker.woff") format("woff");
       }
+      h1{
+        font-family: Haymaker,sans-serif !important;
+        font-weight: 100;
+        font-size: 50px;
+        margin: 20px auto;
+        text-align: center;
+      }
+      ::placeholder { 
+        color: #ccc !important;
+        font-size: 0.8rem;
+        font-weight: 100px;
+      }
+
+      .login {
+        min-width: 315px;
+        background: #F9F9F9;
+        padding: 20px;
+        border-radius: 15px;
+        display: block;
+        margin: 2px auto;
+        box-shadow: #0000002b 0px 0px 9px 2px;
+        max-width: 400px;
+      }
+      label.top_text {
+        display: block;
+        text-align: center;
+        color: #478698;
+        font-size: 20px;
+        font-weight: 900;
+        vertical-align: middle;
+        padding-bottom: 10px;
+      }
+      .top_text img{
+        width: 30px;
+        vertical-align: middle;
+        margin-right: 10px;
+      }
+      input.inp {
+        font-family: "MaisonNeue", "Segoe UI", "Helvetica Neue", -apple-system, system-ui, BlinkMacSystemFont, Roboto, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        font-weight: inherit;
+        color: #63578a !important;
+        font-weight: 600;
+        min-width: 80px;
+        display: block;
+        width: 100%;
+        height: 42px;
+        padding: 10px 30px;
+        font-size: 14px;
+        line-height: 1.42857143;
+        background-color: #fff;
+        background-image: none;
+        border: 1px solid #dfdfe8;
+        border-radius: 20px;
+        -webkit-box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%);
+        box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%);
+        -webkit-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+        -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+        transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+        transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+        transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+      }
+      a.login_btn {
+        margin-top: 30px;
+        padding: 10px 30px;
+        background: #589aca;
+        border-radius: 5px;
+        display: block;
+        color: white;
+        font-weight: 100;
+        font-size: 15px;
+      }
+      .firebaseui-card-footer {
+            display: none;
+        }
       .firebaseui-card-header{
-          display: none;
-      }
+            display: none;
+        }
       .firebaseui-container{
-          background: none;
-      }
+            background: none;
+        }
       .mdl-shadow--2dp{
-          box-shadow: none;
-      }
+            box-shadow: none;
+        }
       .firebaseui-phone-number {
-    display: flex;
-    background: white;
-    border-radius: 6px;
-    border: #efeeee solid 2px;
-    padding: 0px 30px;
-    }
-    .firebaseui-form-actions {
+        display: flex;
+        background: white;
+        border-radius: 6px;
+        border: #efeeee solid 2px;
+        padding: 0px 30px;
+      }
+      .firebaseui-form-actions {
+        text-align: center !important;
+      }
+      .mdl-button--raised.mdl-button--colored:hover {
+        background-color: #6190b7;
+      }
+      .mdl-button--raised.mdl-button--colored {
+        background: #3b7eb7;
+        color: rgb(255,255,255);
+        text-transform: none;
+        border-radius: 8px;
+        font-size: 16px;
+        height: 40px;
+        padding: 0px 110px;
+        font-weight: 400;
+      }
+      .firebaseui-button{
+      margin: 0;
+      }
+      p.price {
+        position: relative;
+        top: -50px;
+        color: red;
+        font-size: 20px;
+        font-weight: bold;
+        margin: 0;
+      }
+      img.img_product {
+        display: block;
+        width: 100%;
+        margin: 0 auto;
+        border: #efeeee solid 2px;
+        border-radius: 20px;
+      }
+      .info p {
+        color: #aaaaaa;
+        font-size: 14px;
+        padding: 0px 20px;
+        float: left;
+        border-right: #dcdcdc solid 2px;
+      }
+      .active, .tel{
+        border-right: none !important; 
+      }
+      .tel{
+        float:none;
+        text-align: center;
+        font-size: 16px;
+        color: #999999;
+      }
+      .info p label {
+        color: rgb(84 84 84);
+        padding-top: 5px;
+        display: block;
+      }
+      .active label {
+        color: #53b52a !important;
+        font-weight: 500;
+      }
+      .tel a{
+        color: #ffffff;
+        background: #87a6de;
+        padding: 10px;
+        display: block;
+        text-align: center;
+        border-radius: 5px;
+        text-decoration: none;
+        font-weight: 500;
+        font-size: 20px;
+        margin: 6px 60px;
+      }
 
-    text-align: center !important;
 
-    }
-    .mdl-button--raised.mdl-button--colored:hover {
-    background-color: #6190b7;
-    }
-    .mdl-button--raised.mdl-button--colored {
-    background: #3b7eb7;
-    color: rgb(255,255,255);
-    text-transform: none;
-    border-radius: 8px;
-    font-size: 16px;
-    height: 40px;
-    padding: 0px 110px;
-    font-weight: 400;
-    }
-    .firebaseui-button{
-    margin: 0;
-    }
-    p.price {
-    position: relative;
-    top: -50px;
+      .icon_smartphone{
+        background-image: url(/icon/smartphone.svg);
+        width: 20px;
+        height: 20px;
+        background-size: 20px;
+        display: inline-block;
+        position: relative;
+        top: 3px;
+        margin-right: 10px;
+      }
+      .info {
+        padding-bottom: 20px;
+        width: 100%;
+        margin: 0 auto;
+      }
+      p.name_product a{
+        font-size: 20px;
+        font-weight: 500;
+        color:#478698;
+        text-decoration: none;
+      }
+      .img {
+        clear: both;
+      }
+      p.name_product {
+        text-align: center;
+      }
 
-    color: red;
-    font-size: 20px;
-    font-weight: bold;
-    margin: 0;
-    }
-    img.img_product {
-    display: block;
-    width: 100%;
-    margin: 0 auto;
-    border: #efeeee solid 2px;
-    border-radius: 20px;
-    }
-    p.date_product {
-    color: #aaaaaa;
-    font-size: 14px;
-    padding: 0px 26px;
-    float: left;
-    font-weight: 400;
-    border-right: #dcdcdc solid 2px;
-    }
-    p.date_product.last{
-    border-right: none;
-    }
-    p.date_product label {
-    color: rgb(84 84 84);
-    padding-top: 5px;
-    display: block;
-    font-weight: 600;
-    }
-    p.date_product.last.active label {
-    color: #75b54d;
-    }
-    .info {
-    padding-bottom: 20px;
-    width: 100%;
-    margin: 0 auto;
-    }
-    p.name_product a{
+      .border{
+        margin: 10px 0;
+        border-bottom: #e2e2e2 solid 2px;
+        display: block;
+      }
+      .mdl-button--raised.mdl-button--colored {
+        background: #3b7eb7;
+        color: rgb(255,255,255);
+        text-transform: none;
+        border-radius: 8px;
+        font-size: 16px;
+        height: 40px;
+        padding: 0px 110px;
+        font-weight: 400;
+        display: block;
+      }
+      @media screen and (max-width: 992px) {
+      .info p {
+        color: #aaaaaa;
+        font-size: 14px;
+        padding: 0px 18px;
+        float: left;
+        font-weight: 400;
+        border-right: #dcdcdc solid 2px;
+      }
+      }
 
-    font-size: 20px;
-    font-weight: 500;
-    color:#478698;
-    text-decoration: none;
-    }
-    .img {
-    clear: both;
-    }
-    p.name_product {
-    text-align: center;
-    }
-
-    .border{
-    margin: 10px 0;
-    border-bottom: #e2e2e2 solid 2px;
-    display: block;
-    }
-    .mdl-button--raised.mdl-button--colored {
-    background: #3b7eb7;
-    color: rgb(255,255,255);
-    text-transform: none;
-    border-radius: 8px;
-    font-size: 16px;
-    height: 40px;
-    padding: 0px 110px;
-    font-weight: 400;
-    display: block;
-    }
-    @media screen and (max-width: 992px) {
-    p.date_product {
-      color: #aaaaaa;
-      font-size: 14px;
-      padding: 0px 20px;
-      float: left;
-      font-weight: 400;
-      border-right: #dcdcdc solid 2px;
-    }
-    }
-
-    /* On screens that are 600px or less, set the background color to olive */
-    @media screen and (max-width: 600px) {
-    p.date_product {
-      color: #aaaaaa;
-      font-size: 14px;
-      padding: 0px 15px;
-      float: left;
-      font-weight: 400;
-      border-right: #dcdcdc solid 2px;
-    }
-    }
+      /* On screens that are 600px or less, set the background color to olive */
+      @media screen and (max-width: 600px) {
+      .info p {
+        color: #aaaaaa;
+        font-size: 14px;
+        padding: 0px 10px;
+        float: left;
+        font-weight: 400;
+        border-right: #dcdcdc solid 2px;
+      }
+      }
     </style>
   `
 
@@ -413,16 +438,17 @@ exports.generateSuccessActivationHTML = function(details) {
                 <h1>POGROM</h1>
                 <!-- Hero Title Info -->
                 <div class="login">
-                    <label class="top_text"><img src="/img/logo_pogrom.svg" alt="logo">Hello</label>
+                
+                <p class="tel">Номер телефона владельца <br><a class="ico" href="tel:${phoneNumber}"><i class="icon_smartphone"></i>${phoneNumber}</a></p>
                     <span class="border"></span>
                     <div class="description">
                         <p class="name_product"><label><a href="https://www.instagram.com/pgrm.clo/">${product.name}</a></label></p>
                     </div>
                     <div class="info">
-                        <p class="date_product">Дата создания: <br><label>${product.creation}</label></p>
-                        <p class="date_product lastPrice">Цена: <br><label>${product.price} грн.</label></p>
-                        <p class="date_product last active">Активация <br><label>Активирован</label></p>
-                        <p class="date_product last active">Номер телефона владельца <br><label>${phoneNumber}</label></p>
+                        <p class="date">Дата покупки: <br><label>${product.creation}</label></p>
+                        <p class="lastPrice">Цена: <br><label>${product.price} грн.</label></p>
+                        <p class="active">Активация <br><label>Активирован</label></p>
+                        
                         
                         <div style="clear:both;"></div>
                     </div>
@@ -529,36 +555,40 @@ exports.generateSuccessActivationHTML = function(details) {
       </script>
       </head>
       ${getCss()}
+     
       <body>
-          <div class="container">
-              <!-- Hero Title -->
-              <h1>POGROM</h1>
-              <!-- Hero Title Info -->
-              <div class="login">
-                  <label class="top_text"><img src="/img/logo_pogrom.svg" alt="logo">Реєстрація</label>
+            <div class="container">
+                <!-- Hero Title -->
+                <h1>POGROM</h1>
+                <!-- Hero Title Info -->
+                <div class="login">
+                
+                <label class="top_text"><img src="/img/logo_pogrom.svg" alt="logo">Реєстрація</label>
                   <span class="border"></span>
                   <div id="firebaseui-auth-container"></div>
-                  <div class="description">
-                      <p class="name_product"><label><a href="https://www.instagram.com/pgrm.clo/">${product.name}</a></label></p>
-                  </div>
-                  <div class="info">
-                      <p class="date_product">Дата создания: <br><label>${product.creation}</label></p>
-                      <p class="date_product lastPrice">Цена: <br><label>${product.price} грн.</label></p>
-                      <p class="date_product last active">Активация <br><label>Не активирован</label></p>
-                      <div style="clear:both;"></div>
-                  </div>
-                  <div style="clear:both;"></div>
-                  <div class="img">
-                      <a href="https://www.instagram.com/pgrm.clo/">
-                          <img class="img_product" src="${product.image}" />
-                      </a>
-                      
-                  </div>
-                  
-                  
-              </div>
-          </div>
-      </body>
+                    <div class="description">
+                        <p class="name_product"><label><a href="https://www.instagram.com/pgrm.clo/">${product.name}</a></label></p>
+                    </div>
+                    <div class="info">
+                        <p class="date">Дата покупки: <br><label>${product.creation}</label></p>
+                        <p class="lastPrice">Цена: <br><label>${product.price} грн.</label></p>
+                        <p class="active">Активация <br><label>Не Активирован</label></p>
+                        
+                        
+                        <div style="clear:both;"></div>
+                    </div>
+                    <div style="clear:both;"></div>
+                    <div class="img">
+                        <a href="https://www.instagram.com/pgrm.clo/">
+                            <img class="img_product" src="${product.image}" />
+                        </a>
+                        
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        </body>
   </html>
       `
   }
